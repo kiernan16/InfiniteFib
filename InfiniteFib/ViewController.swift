1ad38448adb806f6a8675eaf00f80c69844cbfda
Matt Kiernan <kiernan@mail.usf.edu>
2015-07-16 14:46:30 -0400

    Changed to include up to 127127879743834334146972278486287885163 Stops at that Some numbers will miss a '0' placeholder

--- InfiniteFib/ViewController.swift
+++ InfiniteFib/ViewController.swift
@@ -7,17 +7,31 @@
 //
 
 import UIKit
+import Foundation
+
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // MARK: - Variables
     @IBOutlet var tableView: UITableView!
     
-    var alphaNum: CUnsignedLongLong = 0
-    var betaNum: CUnsignedLongLong = 1
-    var fibNum: CUnsignedLongLong = 0
-    var numbers: [CUnsignedLongLong] = [0]
-    var fibDirectionUP = true
+//    var alphaNum: CUnsignedLongLong = 0
+//    var betaNum: CUnsignedLongLong = 1
+//    var fibNum: CUnsignedLongLong = 0
+//    var numbers: [CUnsignedLongLong] = [0]
+//    var fibDirectionUP = true
+    
+    var MAXPLACEHOLDER: CUnsignedLongLong = 10000000000000000000
+    var fibAlpha: CUnsignedLongLong = 0
+    var fibBeta: CUnsignedLongLong = 0
+    var alpha1: CUnsignedLongLong = 0
+    var alpha2: CUnsignedLongLong = 0
+    var beta1: CUnsignedLongLong = 0
+    var beta2: CUnsignedLongLong = 1
+    var temp1: CUnsignedLongLong = 0
+    var temp2: CUnsignedLongLong = 0
+    var numbers1: [CUnsignedLongLong] = [0,0]
+    var numbers2: [CUnsignedLongLong] = [0,1]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +39,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.delegate = self
         tableView.dataSource = self
         
-        while(numbers.count<25){ //loads 25 fibonacci numbers to the 'numbers' array on load
+        while(numbers2.count<25){ //loads 25 fibonacci numbers to the 'numbers' array on load
             self.appendFib()
         }
         
@@ -36,20 +50,47 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // MARK:  Fibonacci Methods
     func nextFib() -> Void { //Finds next Fibonacci number
         
-        //***** LARGEST POSSIBLE FIBONACCI NUMBER ON IOS: 12200160415121876738 ******//
+        //***** LARGEST POSSIBLE FIBONACCI NUMBER ON IOS(ULL): 12200160415121876738 ******//
         
-        if((CUnsignedLongLong.max - betaNum) > alphaNum){
+//        if((CUnsignedLongLong.max - betaNum) > alphaNum){
+        /*
             fibNum = alphaNum + betaNum
             alphaNum = betaNum
             betaNum = fibNum
+        */
     
-        } else{ //Go back down
-            fibDirectionUP = false
-            betaNum = alphaNum
-            println("MAX NUMBER HIT. REVERSE")
-        }
+//        } else{ //Go back down
+//            fibDirectionUP = false
+//            betaNum = alphaNum
+//            println("MAX NUMBER HIT. REVERSE")
+//        }
+        
+        temp1 = fibAlpha
+        temp2 = fibBeta
+       if((CUnsignedLongLong.max - beta1) > alpha1){
+        fibAlpha = alpha1 + beta1
+        fibBeta = alpha2 + beta2
+        
+//        beta2 = alpha2
+//        beta1 = alpha1
+//        
+//        alpha2 = temp2
+//        alpha1 = temp1
+        
+        alpha1 = beta1
+        alpha2 = beta2
+        
+        beta2 = fibBeta
+        beta1 = fibAlpha
+        
+        self.splitFib()
+        self.splitAlpha()
+        self.splitBeta()
+       }
+        
     }
-    
+
+/*
     func backFib() -> Void { //Finds previous Fibonacci number
         if(alphaNum > 0){
             alphaNum = fibNum - betaNum
@@ -72,21 +113,56 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
         
     }
+*/
     
     //Adds Fibonacci number to 'numbers' array
     func appendFib() -> Void {
         
-        if(fibDirectionUP == true){
+//        if(fibDirectionUP == true){
             self.nextFib()
-        } else {
-            self.backFib()
-        }
-        if(fibNum == 1){    //Adds the "extra" '1' for the series
-            numbers.append(1)
-        }
-        numbers.append(fibNum)
+//        } else {
+//            self.backFib()
+//        }
+//        if(fibNum == 1){    //Adds the "extra" '1' for the series
+//            numbers.append(1)
+//        }
+      //  numbers.append(fibNum)
+        
+        numbers2.append(fibBeta)
+        numbers1.append(fibAlpha)
         
     }
+    
+    func splitFib() -> Void {
+//        if(fibNum >= MAXPLACEHOLDER){
+//            
+//        }
+        
+        fibAlpha += fibBeta/MAXPLACEHOLDER
+        fibBeta -= (fibBeta/MAXPLACEHOLDER)*MAXPLACEHOLDER
+    }
+    
+
+    func splitAlpha() -> Void {
+        alpha1 += alpha2/MAXPLACEHOLDER
+        alpha2 -= (alpha2/MAXPLACEHOLDER)*MAXPLACEHOLDER
+    }
+    
+    func splitBeta() -> Void {
+        beta1 += beta2/MAXPLACEHOLDER
+        beta2 -= (beta2/MAXPLACEHOLDER)*MAXPLACEHOLDER
+    }
+ 
+/*
+    func splitNumber(number1 : CUnsignedLongLong, number2 : CUnsignedLongLong) -> Void {
+        
+        var num1 : CUnsignedLongLong = number1
+        var num2 : CUnsignedLongLong = number2
+        
+        num1 += num2/MAXPLACEHOLDER
+        num2 -= ((num2/MAXPLACEHOLDER)*MAXPLACEHOLDER)
+    }
+*/
     
     // MARK:  UITableViewDelegate Methods
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -110,7 +186,22 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
         self.appendFib()
         
-        cell!.textLabel?.text = String(numbers[row])
+  //      cell!.textLabel?.text = String(numbers[row])
+        // cell!.textLabel?.text = String(format: "1 %ull" ,numbers[row])
+       
+            if(numbers1[row] > 0){
+           // cell!.textLabel?.text = String(format: "%g%04g", arguments: numbers1[row],numbers2[row])
+            //cell!.textLabel?.text = String(format: "%g", arguments: numbers1[row])
+                cell!.textLabel?.text = String("\(numbers1[row])\(numbers2[row])")
+            //println(NSString(format:"%02i", i))
+            //println(String(format: "a float number: %.5f", 1.0321))
+            //cell!.textLabel?.text = NSString(format: "%ll%04ll", numbers1[row],numbers2[row]) as String
+           // cell!.textLabel?.text = String(NSString(format: "\(numbers1[row])%04llu", numbers2[row]))
+            //"%04llu"
+            
+            } else {
+                cell!.textLabel?.text = String(numbers2[row])
+                }
         
         
         return cell!
@@ -120,6 +211,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
         
         let row = indexPath.row
-        println(numbers[row])
+        println("numbers1: \(numbers1[row])")
+        println("numbers2: \(numbers2[row])")
     }
 }
